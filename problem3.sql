ce36556e63d718a3e004a40d4807607b068ff59e
monju1125 <monju1125@github.com>
2026-06-06 06:47:03 +0000

    Test run: 2/3 passed, 1 failed, 0 skipped - 2026-06-06T06:47:03.137Z

--- problem3.sql
+++ problem3.sql
@@ -1,1 +1,1 @@
-SELECT* FROM employee WHERE first_name IN NOT(first_name = 'STEVE') AND NOT (salary < 50000);
+SELECT* FROM employee WHERE first_name NOT IN('STEVE') AND salary NOT IN(salary < 50000);
